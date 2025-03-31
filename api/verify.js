d1543684efffaad43b68b97d78b63932e1571fd1
chandu1333 <108852121+chandu1333@users.noreply.github.com>
2025-03-31 10:18:23 +0530

    Add files via upload

---
+++ api/verify.js
@@ -0,0 +1,17 @@
+const crypto = require("crypto");
+const fs = require("fs");
+const privateKey = fs.readFileSync("./private.pem", "utf8");
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).end("Only POST allowed");
+  }
+
+  const publicKeyFromMeta = req.body.public_key;
+  const sign = crypto.createSign("RSA-SHA256");
+  sign.update(publicKeyFromMeta);
+  sign.end();
+  const signature = sign.sign(privateKey, "base64");
+
+  res.status(200).json({ signature });
+}
